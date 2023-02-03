5a8b4f73f5597a6b20a1c7df9418946480377aa3
HGergo <gergo.hegyi@calv.us>
2023-02-03 20:41:52 +0100

    fix: removing / to be consistent at naming

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3500;
 
 // loading static files like: css, images
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // handling the / route
 app.use('/', require('./routes/root'));
